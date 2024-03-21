3fd109cce7033ad131b2c1e1bdf644267ce4a532
Josef <josef.pluhar@gmail.com>
2024-03-21 21:49:49 +0100

    no message

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Nahrávání obrázku</title>
 </head>
 <body>
-    <h1>Nahrání nového obrázku</h1><br>
+    <h1>Nahrání obrázku</h1><br>
     
     <form action="index.php" method="POST" enctype="multipart/form-data">
         <label for="soubor">Soubor obrázku: </label>
@@ -21,10 +21,21 @@
     
     <?php
     //input
+    $nazev = $_POST['novyNazev'];
+    //$soubor = $_POST['soubor'];
     echo'<pre>';
-    echo var_dump($_FILES);
+   // echo var_dump($_FILES);
+
+    $filename = $_FILES['soubor']['name'];
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    $allowed = array('jpg','png','gif');
+    if( ! in_array( $ext, $allowed ) ) {
+        echo 'error';
+    }
+    echo($ext);
+    
     if(isset($_FILES['soubor'])){//hlidani zda byl soubor skutecne nahran
-        move_uploaded_file($_FILES['soubor']['tmp_name'], ['novyNazev']); //nutnost nahrat obrazek, jinak je docasne ulozeny na serveru v adrese pod tmp_name
+        move_uploaded_file($_FILES['soubor']['tmp_name'], $nazev.'.jpg'); //nutnost nahrat obrazek, jinak je docasne ulozeny na serveru v adrese pod tmp_name
     }
 
 
